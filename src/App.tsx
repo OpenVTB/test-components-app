e64878d41fdafb3c093cbcdf8fdea73c0ca9de44
AYLitvinov <aylitvinov@MBP-AYL.local>
2020-10-27 19:43:44 +0300

    upgrade to cra v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Checkbox, Accordion } from '@openvtb/react-ui-kit';
 
 function App() {
+  const [checked, setCheked] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +12,27 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Checkbox onChange={() => setCheked(!checked)} checked={checked} />
+        <Accordion
+          list={[
+            {
+              id: 0,
+              title: 'Title name',
+              content: 'Lorem ipsum dolor...',
+            },
+            {
+              id: 1,
+              title: 'Title name',
+              content: 'Lorem ipsum dolor...',
+            },
+            {
+              id: 2,
+              title: 'Title name',
+              content: 'Lorem ipsum dolor...',
+            },
+          ]}
+          width={600}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
